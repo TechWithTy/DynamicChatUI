2c07ebc68558e64e8eae77e14c5fc1ce760bca9f
nehal7-ml <nehal.sk.99@gmail.com>
2024-03-23 04:44:56 +0000

    project header

--- components/ProjectHeader/index.tsx
+++ components/ProjectHeader/index.tsx
@@ -1,51 +1,67 @@
 import React from "react";
-// Import only necessary icons here
-import { CircleEllipsis, Share2 } from "lucide-react";
+import { CircleEllipsis, Edit, LucideShieldEllipsis, Share2, ShareIcon, } from "lucide-react";
+import { DisplayUserDTO } from "@/crud/DTOs";
+import MembersIndicator from "./MemberIndicator";
 
 interface HeaderProps {
-  title: string;
-  description: string;
-  tabs: string[];
-  icons: React.ReactNode[]; // Array of React node elements for icons
+    title: string;
+    description: string;
+    tabs: string[];
+    users:  {  firstName?: string;
+        lastName?: string;
+        image?: string;
+        isOnline: boolean}[]
 }
 
-const ProjectHeader: React.FC<HeaderProps> = ({
-  title,
-  description,
-  tabs,
-  icons,
-}) => {
-  return (
-    <header className="rounded-md p-4 dark:bg-gray-800">
-      <div className="flex items-center justify-between">
-        <div>
-          <h1 className="text-xl font-bold">{title}</h1>
-          <p className="text-gray-400">{description}</p>
-          <div className="mt-2 flex">
-            {tabs.map((tab, index) => (
-              <div
-                key={index}
-                className={`mr-4 flex items-center last:mr-0 ${
-                  index === 0 ? "text-green-500" : "text-gray-300"
-                }`}
-              >
-                {icons[index]} {/* Render the passed-in icon */}
-                <span className="ml-1">{tab}</span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <button className="hidden gap-2 rounded-md bg-blue-600 p-2 text-white hover:bg-blue-700 lg:flex dark:bg-gray-600 dark:hover:bg-slate-700">
-          <Share2 size={20} />
-          Share
-        </button>
-        <button className="flex rotate-90 gap-2 rounded-md lg:hidden">
-          <CircleEllipsis size={20} />
-        </button>
-      </div>
-      <hr className="my-2 border-gray-700" />
-    </header>
-  );
+const ProjectHeader: React.FC<HeaderProps> = ({ title, description, tabs, users }) => {
+    return (
+        <header className="p-4  dark:bg-gray-800  rounded-md">
+            <div className="flex items-center justify-between">
+                <div>
+                    <h1 className="text-xl font-bold">{title}</h1>
+                    <p className="text-gray-400">{description}</p>
+                    <div className="mt-2 flex">
+                        {tabs.map((tab, index) => (
+                            <div
+                                key={index}
+                                className={`mr-4 last:mr-0 ${index === 1 ? "text-green-500" : "text-gray-300"
+                                    }`}
+                            >
+                                {tab}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex">
+                    <div className="">
+                        <MembersIndicator additionalCount={5} users={users} />
+                    </div>
+                    <div className="hidden lg:flex lg:gap-2 lg:justify-center lg:items-center">
+                        <button className=" hidden gap-2 rounded-md bg-blue-600 text-white p-2 hover:bg-blue-700 lg:flex justify-center items-center dark:bg-transparent  dark:hover:bg-slate-700 ">
+                            <Share2 size={20} className="" />
+                            <span>Share</span>
+                        </button>
+                        <button className=" hidden gap-2 p-2 rounded-md  hover:text-white hover:bg-blue-700 lg:flex  dark:bg-gray-600 dark:hover:bg-slate-700 ">
+                            <Edit size={20} className="" />
+                        </button>
+                    </div>
+                    <button className=" rotate-90 flex gap-2 rounded-md  lg:hidden ">
+                        <CircleEllipsis />
+                    </button>
+                    <div className="lg:hidden">
+                        <button className="flex gap-2 rounded-md bg-blue-600 text-white p-2 hover:bg-blue-700 lg:hidden dark:bg-gray-600 dark:hover:bg-slate-700 ">
+                            <Share2 size={20} className="" />
+                            Share
+                        </button>
+                        <button>
+                            <Edit />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <hr className="my-2 border-gray-700" />
+        </header>
+    );
 };
 
 export default ProjectHeader;
