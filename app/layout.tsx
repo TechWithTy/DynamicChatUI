2e188ec11c5353c073a8aee9f27feb52b17af68b
Tyrique Daniel <37724661+TechWithTy@users.noreply.github.com>
2024-03-23 04:20:20 +0000

    Removed Navbar padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,17 +101,17 @@ export default async function RootLayout({
           nunito.variable,
         )} 
                         ${
-          theme?.value
-          } relative max-h-screen min-h-screen overflow-x-hidden antialiased`}
+                          theme?.value
+                        } relative max-h-screen min-h-screen overflow-x-hidden antialiased`}
       >
         <div className="fixed h-full w-screen dark:bg-gray-900 dark:text-white " />
         <Suspense fallback="...">
-          <NavBar
+          {/* <NavBar
             session={session}
             darkMode={theme?.value === "dark" ? true : false}
-          />
+          /> */}
         </Suspense>
-        <main className="fixed h-full w-full overflow-x-hidden pt-16 xl:pt-24 dark:bg-gray-900 dark:text-white">
+        <main className="fixed h-full w-full overflow-x-hidden  dark:bg-gray-900 dark:text-white">
           {children}
           <noscript>
             <iframe
